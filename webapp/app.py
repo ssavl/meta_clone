d5cdb27b8678f8e32278d91add752a96aa977f6e
ssavl <get-moscow@yandex.ru>
2021-02-23 19:36:41 +0300

    added cors config

--- webapp/app.py
+++ webapp/app.py
@@ -1,14 +1,34 @@
 from flask import Flask, render_template, request, jsonify
 from db import tasks
-# from flask import render
+from flask_cors import CORS
 
+# configurations
+DEBUG = True
 
+# application
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r"/api": {"origins": "*"}})  # Даем любому ip делать cross-origin-requests TODO: надо изменить!
 
 
-@app.route("/api", methods=['GET'])
+
+
+@app.route("/api", methods=['GET', 'POST'])
 def index():
     return jsonify({'tasks': tasks})
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
--- webapp/models.py
+++ webapp/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String
 from db import Base, engine
 
 
+
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, unique=True)
